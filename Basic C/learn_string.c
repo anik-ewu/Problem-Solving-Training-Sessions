71183c0950d4712003d93834820a397280679130
Sabbir Hasan Anik <sabbir.hasan@selise.ch>
2023-04-27 10:40:08 +0600

    string in c

---
+++ Basic C/learn_string.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+
+    char a;
+    char arr[10];
+
+    ///use gets to take input a line
+    ///gets(arr);
+    scanf("%s", &arr);
+    printf("%s", arr);
+
+    return 0;
+}
