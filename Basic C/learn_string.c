4c1a78d9d12186bb78eb863d0df1f0371c3f2c35
Sabbir Hasan Anik <sabbir.hasan@selise.ch>
2023-04-27 11:11:29 +0600

    update

--- Basic C/learn_string.c
+++ Basic C/learn_string.c
@@ -4,10 +4,12 @@ int main() {
     char a;
     char arr[10];
 
-    ///use gets to take input a line
+    ///use gets to take with spaces
     ///gets(arr);
     scanf("%s", &arr);
     printf("%s", arr);
+
+    ///Bangladesh India Different Country
 
     return 0;
 }
