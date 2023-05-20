292183d077e7b1c70fa11c3afb7256bac5b0bed8
Sabbir Hasan Anik <sabbir.hasan@selise.ch>
2023-05-20 12:20:25 +0600

    Array

---
+++ Basic C/11.array-1d.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+
+int main() {
+
+    int a, b;
+    int arr[100];
+
+    ///assign value statically
+    /*
+    arr[0] = 5;
+    arr[1] = 10;
+    arr[2] = 100;
+    printf("%d\n", arr[0]);
+    printf("%d\n", arr[1]);
+    printf("%d\n", arr[2]);
+    */
+
+    ///array input
+    /*
+    scanf("%d", &arr[3]);
+    scanf("%d", &arr[4]);
+    printf("%d\n", arr[3]);
+    printf("%d\n", arr[4]);
+    */
+
+
+
+
+    ///Input values of array using loop
+
+    /*for (int i = 1; i <= 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+    ///Print all numbers
+    for (int i = 1; i <= 5; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    ///print only number of index 4
+    printf("%d ", arr[4]);*/
+
+    /*
+    Exercise:
+    1. Reverse
+        FOR (I N to 1) {
+            PRINT ARRAY
+        }
+    2. Count Even
+        INT EVEN: 0
+        FOR (I 1 to N) {
+            IF NUM IS Even
+                EVEN: EVEN + 1
+        }
+    3. Same as 2
+    4. Find Maximum
+        INT MAX: ARRAY[0]
+        FOR (I 1 to N) {
+            IF NUM IS Greater than max
+                update the value of max
+        }
+    5. Same as 4
+    */
+
+
+    return 0;
+}
